23a71656f9e78cf61fe7eb271ee27a6486f5bfc0
Yuliia Kravchenko <yukravch@bess-f2r4s19.clusters.42paris.fr>
2025-02-23 14:53:28 +0100

    Validated a mandatory part of the project
    2 static variables used, reading from only one fd

---
+++ mandatory_part/get_next_line_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 12:13:18 by yukravch          #+#    #+#             */
+/*   Updated: 2024/12/18 12:26:45 by yukravch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*array;
+
+	array = malloc(nmemb * size);
+	if (!array)
+		return (NULL);
+	ft_bzero(array, nmemb * size);
+	return (array);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlen_line(char *line)
+{
+	size_t	i;
+
+	i = 0;
+	if (line == NULL)
+		return (0);
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	if (line[i] == '\n')
+		i++;
+	return (i);
+}
+
+int	ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
